6842b2320569579439d88ac83c0e8c100fb88e2a
Carlos Rafael González Perdomo <carlosrafael@MacBook-Air-de-Carlos.local>
2024-03-01 18:52:14 -0500

    layout overview header

--- src/components/Overview/index.tsx
+++ src/components/Overview/index.tsx
@@ -1,7 +1,11 @@
+import Icon from './chart-icon.svg';
+
 export function Overview() {
     return (
-        <section className="container bg-secondary rounded-xl">
-            <h2>Overview</h2>
+        <section className="container bg-secondary rounded-xl py-4 px-5">
+            <header className="flex gap-2 mb-4">
+                <img src={Icon} alt="Leaderboard icon" /><h2 className="text-parchment font-bold text-lg leading-27">Overview</h2>
+            </header>
         </section>
     );
 }
