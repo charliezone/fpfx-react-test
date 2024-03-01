d4e71df4ac4418f39d542fd1cac5756247473bfd
Carlos Rafael González Perdomo <carlosrafael@MacBook-Air-de-Carlos.local>
2024-03-01 18:20:12 -0500

    fix balance column color

--- src/components/Leaderboard/LeaderTable/index.tsx
+++ src/components/Leaderboard/LeaderTable/index.tsx
@@ -14,28 +14,28 @@ export function LeaderTable() {
                 </thead>
                 <tbody>
                     <tr className="odd:bg-cyprus even:bg-daintree border-b border-primary last:border-b-0">
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">Martin Newman</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$980,000</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$265,000</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$715,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">Martin Newman</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">$980,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">$265,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-java">$715,000</td>
                     </tr>
                     <tr className="odd:bg-cyprus even:bg-daintree border-b border-primary last:border-b-0">
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">Martin Newman</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$980,000</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$265,000</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$715,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">Martin Newman</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">$980,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">$265,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-java">$715,000</td>
                     </tr>
                     <tr className="odd:bg-cyprus even:bg-daintree border-b border-primary last:border-b-0">
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">Martin Newman</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$980,000</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$265,000</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$715,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">Martin Newman</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">$980,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">$265,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-java">$715,000</td>
                     </tr>
                     <tr className="odd:bg-cyprus even:bg-daintree border-b border-primary last:border-b-0">
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">Martin Newman</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$980,000</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$265,000</td>
-                        <td className="pl-5 last:pr-5 py-3 font-semibold text-md text-cloudy">$715,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">Martin Newman</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">$980,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-cloudy">$265,000</td>
+                        <td className="pl-5 last:pr-5 py-3 font-semibold text-sm text-java">$715,000</td>
                     </tr>
                 </tbody>
             </table>
